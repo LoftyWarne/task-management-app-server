adb5ca2a4363f9125387b74e6a5ba75f65a7a42c
LoftyWarne <conorwarne92@gmail.com>
2023-01-23 20:58:51 +0000

    added Cron Job to log overdue tasks

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
+const axios = require('axios')
+const cron = require("node-cron");
 const express = require("express");
 const cors = require("cors");
+const task = require("./app/controllers/task.controller.js");
 
 const app = express();
 
@@ -22,6 +25,25 @@ app.get("/", (req, res) => {
 
 require("./app/routes/list.routes")(app);
 require("./app/routes/task.routes")(app);
+
+// Log overdue tasks on schedule at 9am every day to mock email service
+cron.schedule("0 9 * * *", async () => {
+    console.log('You will see this message every second');
+
+    try {
+      let resp = await axios.get(process.env.API_ADDRESS)
+      const data = resp.data
+      let overdueTasks = ""
+      data.forEach(obj => {
+        overdueTasks += obj.tbl_TaskName + ", "
+      })
+      overdueTasks = overdueTasks.trimEnd()
+      overdueTasks = overdueTasks.substring(0, overdueTasks.length - 1)
+      console.log(`The following tasks are now overdue:${overdueTasks}`)
+    } catch (error) {
+      console.error(error)
+    }
+})
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
